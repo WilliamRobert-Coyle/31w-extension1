eb77b6ec9358d469e925548fbf98933f398b0bc8
William Robert-Coyle <willrobertcoyle22@gmail.com>
2022-06-02 11:41:19 -0500

    S3-c2 configuration de notre premier plugin

---
+++ ex1_31w.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * plugin Name: Extension de l'exerice #1
+ * Author: Eddy MArtin
+ * Description: Une premiere extension pour l'exercice #1
+ * Author URI: https://github.com/WilliamRobert-Coyle
+ */
+?>
+
+<?php
+?>
